aaadfdd7094e78959e416bf3b1a96705d55676ed
Risabh Raj <nitianrisabh12@gmail.com>
2021-04-08 01:00:25 -0700

    Change welcome page to home page

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome, name="welcome"),
+    path('', views.home, name="welcome"),
     path('home/', views.home, name="home"),
     path('products/', views.productshome, name="products-home"),
     path('services/', views.serviceshome, name="services-home"),
