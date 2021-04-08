a78e2ff25e35999d88f3144359c3a0121c2f6cdf
Risabh Raj <nitianrisabh12@gmail.com>
2021-04-08 00:36:12 -0700

    Add Product model

--- products/models.py
+++ products/models.py
@@ -2,9 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a product category (e.g. Electronics)')
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     product_id = models.CharField(max_length=100)
     username = models.ForeignKey(User, on_delete=models.CASCADE)
-    photo_id = models.CharField(max_length=64)
-    available = models.BooleanField()
+    product_name = models.CharField(max_length=100)
     description = models.TextField()
+    photo_id = models.CharField(max_length=64)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product_name
+
+# class ProductFollow(models.Model):
+#     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#
+#
+# class Barter(models.Model):
+#     barter_id = models.CharField(max_length=100)
+#     status = models.BooleanField(default=False)
+#     user1 = models.ForeignKey()
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def welcome(request):
@@ -7,17 +8,20 @@ def welcome(request):
     }
     return render(request, 'products/welcome.html', context)
 
+
 def home(request):
     context = {
         'goods': ['Home page'],
     }
     return render(request, 'products/home.html', context)
 
+
 def productshome(request):
     context = {
-        'goods': ['Products page'],
+        'products': Product.objects.all()
     }
     return render(request, 'products/productshome.html', context)
+
 
 def serviceshome(request):
     context = {
