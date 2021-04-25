ecf35e866bc161046a6405c4c2b67e8163799968
Risabh Raj <nitianrisabh12@gmail.com>
2021-04-25 11:51:08 -0700

    Remove redundant '/sendmsg' route
    Will use '/chat' route for all communication

--- django_project/urls.py
+++ django_project/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('chat/', user_views.chat, name='chat'),
-    path('sendmsg/', user_views.sendmsg, name='send-message'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -43,50 +43,16 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
-'''
+
 @login_required
 def chat(request):
     receiver_name = request.GET.get('receiver')
 
     other_user = User.objects.get(username=receiver_name)
     current_user = request.user
-    context = {}
-    if request.method == 'GET':
-        context['msg_history'] = Message.objects.filter(
-            (Q(sender=current_user) & Q(receiver=other_user)) |
-            (Q(sender=other_user) & Q(receiver=current_user))).order_by('created_at')
-
-    return render(request, 'users/chat.html', context)
-
-
-@login_required
-def sendmsg(request):
-    receiver = request.GET.get('receiver')
-    if request.method == 'POST':
-        msg_instance = Message(sender=request.user, receiver=User.objects.get(username=receiver))
-        form = SendMsgForm(request.POST, instance=msg_instance)
-        if form.is_valid():
-            form.save()
-            messages.success(request, f'Message sent to: {receiver}')
-            return redirect('products-home')
-    else:
-        form = SendMsgForm()
-
-    return render(request, 'users/sendmsg.html', {'form': form})
-'''
-
-    #///////////////////////////Trying Combination//////////////
-
-@login_required
-def sendmsg(request):
-    receiver_name = request.GET.get('receiver')
-
-    other_user = User.objects.get(username=receiver_name)
-    current_user = request.user
-    context1 = {}
-    context1['msg_history'] = Message.objects.filter(
-    (Q(sender=current_user) & Q(receiver=other_user)) |
-    (Q(sender=other_user) & Q(receiver=current_user))).order_by('created_at')
+    msg_data = {'msg_history': Message.objects.filter(
+        (Q(sender=current_user) & Q(receiver=other_user)) |
+        (Q(sender=other_user) & Q(receiver=current_user))).order_by('created_at')}
 
     if request.method == 'POST':
         msg_instance = Message(sender=request.user, receiver=User.objects.get(username=receiver_name))
@@ -99,38 +65,8 @@ def sendmsg(request):
         form = SendMsgForm()
 
     context = {
-            'context1': context1,
-            'form': form
-        }
+        'msg_data': msg_data,
+        'form': form
+    }
 
     return render(request, 'users/chat.html', context)
-
-
-@login_required
-def chat(request):
-    receiver_name = request.GET.get('receiver')
-
-    other_user = User.objects.get(username=receiver_name)
-    current_user = request.user
-    context1 = {}
-    context1['msg_history'] = Message.objects.filter(
-    (Q(sender=current_user) & Q(receiver=other_user)) |
-    (Q(sender=other_user) & Q(receiver=current_user))).order_by('created_at')
-
-    if request.method == 'POST':
-        msg_instance = Message(sender=request.user, receiver=User.objects.get(username=receiver_name))
-        form = SendMsgForm(request.POST, instance=msg_instance)
-        if form.is_valid():
-            form.save()
-            messages.success(request, f'Message sent to: {receiver_name}')
-            return redirect('products-home')
-    else:
-        form = SendMsgForm()
-
-    context = {
-            'context1': context1,
-            'form': form
-        }
-
-    return render(request, 'users/chat.html', context)
-
